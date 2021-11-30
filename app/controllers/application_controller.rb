3e268e304400add263afa18203e1d8d0e7e55e5b
yoshitarotsuda <tsuda.shinden@gmail.com>
2021-11-30 18:17:16 +0900

    BASIC認証導入

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,12 @@
 class ApplicationController < ActionController::Base
+  before_action :basic_auth
+  
+  private
+
+  def basic_auth # BASIC認証設定
+    authenticate_or_request_with_http_basic do |username, password|
+      username == ENV["BASIC_AUTH_USER"] && password == ENV["BASIC_AUTH_PASSWORD"]
+    end
+  end
+
 end
