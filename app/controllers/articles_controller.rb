9300f197678228a3559807d8d37fd28a5503d530
yoshitarotsuda <tsuda.shinden@gmail.com>
2021-12-07 18:34:06 +0900

    記事の保存確認

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -64,6 +64,6 @@ class ArticlesController < ApplicationController
 
     # Only allow a list of trusted parameters through.
     def article_params
-      params.require(:article).permit(:title,:content)
+      params.require(:article).permit(:title,:content).merge(user_id: current_user.id)
     end
 end
