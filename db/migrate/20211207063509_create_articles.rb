659bca7c0e46749e59906c50fb2cb5ce344bdd81
yoshitarotsuda <tsuda.shinden@gmail.com>
2021-12-07 15:56:37 +0900

    deviseインストール、マイグレーションファイル編集

--- db/migrate/20211207063509_create_articles.rb
+++ db/migrate/20211207063509_create_articles.rb
@@ -1,8 +1,8 @@
 class CreateArticles < ActiveRecord::Migration[6.0]
   def change
     create_table :articles do |t|
-      t.string :title
-
+      t.string :title,            null: false
+      t.references :user,         null: false, foreign_key: true # 外部キー設定
       t.timestamps
     end
   end
