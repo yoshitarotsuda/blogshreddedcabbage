9b29c24162a2d2d79d7ae4b1d8a76e8ca5e35c12
yoshitarotsuda <tsuda.shinden@gmail.com>
2021-12-07 16:27:10 +0900

    Articlesモデル　アソシエーション、バリデーション設定

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,2 +1,9 @@
 class Article < ApplicationRecord
+  belongs_to :user
+  has_rich_text :content
+
+  with_options presence: true do #with_options オプションの記述をまとめるのに使える、if使用時の記述パターンに注意
+    validates :title
+    validates :content
+  end
 end
