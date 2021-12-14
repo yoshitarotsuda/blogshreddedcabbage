fe422864a7b45de7b083efebe7064e583d936763
yoshitarotsuda <tsuda.shinden@gmail.com>
2021-12-14 12:19:42 +0900

    サムネイル登録機能追加

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,5 +1,6 @@
 class Article < ApplicationRecord
   belongs_to :user
+  has_one_attached :image
   has_rich_text :content
 
   with_options presence: true do #with_options オプションの記述をまとめるのに使える、if使用時の記述パターンに注意
