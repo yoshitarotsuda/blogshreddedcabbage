6787da803e7b1bbccac1ac2694dbac5f7d94bb6e
yoshitarotsuda <tsuda.shinden@gmail.com>
2021-12-15 11:27:04 +0900

    タグ関連アソシエーション設定

--- app/models/article.rb
+++ app/models/article.rb
@@ -2,6 +2,8 @@ class Article < ApplicationRecord
   belongs_to :user
   has_one_attached :image
   has_rich_text :content
+  has_many :article_tags, dependent: :destroy
+  has_many :tags,through: :article_tags
 
   with_options presence: true do #with_options オプションの記述をまとめるのに使える、if使用時の記述パターンに注意
     validates :title
